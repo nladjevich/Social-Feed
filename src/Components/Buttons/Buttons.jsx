dc69749c7227b1b2d467d96ab52157dd419f7c38
Nicholas <zantladjevich@gmail.com>
2022-12-13 10:21:30 -0500

    Created Buttons component to display a like and dislike button to each post

---
+++ src/Components/Buttons/Buttons.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Buttons = (props) => {
+    return ( 
+        <div>
+            <button type='button'>Like</button>
+            <button type='button'>Dislike</button>
+        </div>
+        
+     );
+}
+ 
+export default Buttons;props
